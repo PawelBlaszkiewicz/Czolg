c26235e00a04820687e37aafebde623cb45c8e42
PawelBlaszkiewicz <pawelbfoto@gmail.com>
2021-01-11 18:31:55 +0100

    Mała poprawa pamięci

--- interpretacja.c
+++ interpretacja.c
@@ -5,14 +5,20 @@ int daj_x(cJSON *cus)
 {
     cJSON *pay = cJSON_GetObjectItemCaseSensitive(cus, "payload");
     cJSON *name = cJSON_GetObjectItemCaseSensitive(pay, "current_x");
-    return atof(cJSON_Print(name));
+    char *ah=cJSON_Print(name);
+    int a=atoi(ah);
+    cJSON_free(ah);
+    return a;
 }
 
 int daj_y(cJSON *cus)
 {
     cJSON *pay = cJSON_GetObjectItemCaseSensitive(cus, "payload");
     cJSON *name = cJSON_GetObjectItemCaseSensitive(pay, "current_y");
-    return atof(cJSON_Print(name));
+    char *ah=cJSON_Print(name);
+    int a=atoi(ah);
+    cJSON_free(ah);
+    return a;
 }
 
 char * daj_pole(cJSON *cus)
@@ -32,13 +38,19 @@ char * daj_kier(cJSON *cus)
 int daj_xe(cJSON *cus)
 {
     cJSON *name = cJSON_GetObjectItemCaseSensitive(cus, "x");
-    return atof(cJSON_Print(name));
+    char *ah=cJSON_Print(name);
+    int a=atoi(ah);
+    cJSON_free(ah);
+    return a;
 }
 
 int daj_ye(cJSON *cus)
 {
     cJSON *name = cJSON_GetObjectItemCaseSensitive(cus, "y");
-    return atof(cJSON_Print(name));
+    char *ah=cJSON_Print(name);
+    int a=atoi(ah);
+    cJSON_free(ah);
+    return a;
 }
 
 char * daj_type(cJSON *cus)
@@ -49,32 +61,40 @@ char * daj_type(cJSON *cus)
 
 int zwroc_nr_pola(cJSON *a)
 {
-    if(strcmp(daj_pole(a), "\"grass\"" )==0)
+    char *pol=daj_pole(a);
+    if(strcmp(pol, "\"grass\"" )==0)
         {
+            cJSON_free(pol);
             return 1;
         }
-        else if(strcmp(daj_pole(a),"\"sand\"")==0)
+        else if(strcmp(pol,"\"sand\"")==0)
         {
+            cJSON_free(pol);
             return 2;
         }
-        else if(strcmp(daj_pole(a),"\"wall\"")==0)
+        else if(strcmp(pol,"\"wall\"")==0)
         {
+            cJSON_free(pol);
             return 3;
         }
 }
 
 int zwroc_enr_pola(cJSON *a)
 {
-    if(strcmp(daj_type(a), "\"grass\"" )==0)
+        char *pol=daj_type(a);
+        if(strcmp(pol, "\"grass\"" )==0)
         {
+            cJSON_free(pol);
             return 1;
         }
-        else if(strcmp(daj_type(a),"\"sand\"")==0)
+        else if(strcmp(pol,"\"sand\"")==0)
         {
+            cJSON_free(pol);
             return 2;
         }
-        else if(strcmp(daj_type(a),"\"wall\"")==0)
+        else if(strcmp(pol,"\"wall\"")==0)
         {
+            cJSON_free(pol);
             return 3;
         }
 }
